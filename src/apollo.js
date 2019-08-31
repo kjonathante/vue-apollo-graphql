b5b5467afac17c0f0bd8fd628cd908999e5bb888
Kit Jonathan Te <kjonathante@gmail.com>
2019-08-31 10:20:05 -0700

    Manually installed Apollo Client.

---
+++ src/apollo.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+Vue.use(VueApollo)
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://us1.prisma.sh/kjonathante-151c9e/hello-world/dev'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+export { apolloProvider }
