4e430a2f8a185509563f9431f91ae3bed35ab2f8
Kit Jonathan Te <kjonathante@gmail.com>
2019-09-04 16:23:27 -0700

    Added a Card component to test the reactivity of the local state.

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="div" v-bind:style="{ backgroundColor: global.property1 }">
+    <p>This is a card</p>
+    <p>{{ global }}</p>
+  </div>
+</template>
+
+<script>
+import { globalProperty1Query } from '../state/queries'
+
+export default {
+  apollo: {
+    global: globalProperty1Query
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.div {
+  width: 150px;
+  padding: 5px;
+  border: 1px solid darkgray;
+}
+</style>
--- src/views/LocalState.vue
+++ src/views/LocalState.vue
@@ -3,10 +3,12 @@
     <p>{{ global }}</p>
     <input type="text" v-model="text" />
     <button v-on:click="updateProperty">Change</button>
+    <card />
   </div>
 </template>
 <script>
 import { globalProperty1Query, globalProperty1Mutation } from '../state/queries'
+import card from '../components/Card'
 
 export default {
   data() {
@@ -28,6 +30,10 @@ export default {
         variables: { value: this.text }
       })
     }
+  },
+
+  components: {
+    card
   }
 }
 </script>
